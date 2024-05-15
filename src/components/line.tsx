803a51716fe9cb7bfba03bf3d81fda6cd9f9a8fc
Arthur Fellipe <arthur.fellipe.brandao@gmail.com>
2024-05-15 13:53:55 -0300

    Atualização

--- src/components/line.tsx
+++ src/components/line.tsx
@@ -15,14 +15,14 @@ export const Line = (props: LineProps) => {
 
     console.log("Todo-list",props.checked);
     return (
-        <div id="dado" className="bg-zinc-800 h-[8vh] w-[70vh]rounded text-zinc-500 flex items-center p-5 gap-3 text-xs font-bold">
+        <div id="dado"  onClick={() => props.handleClick(props.index)} className=" cursor-pointer bg-zinc-800 h-[8vh] w-[70vh]rounded text-zinc-500 flex items-center p-5 gap-3 text-xs font-bold">
             {
                 props.checked ? (
-                    <button onClick={() => props.handleClick(props.index)} className="w-[1.5rem] h-[1.5rem] rounded-full border border-gray-500 bg-gradient-to-l from-pink-500 to-purple-700 text-white flex items-center justify-center">
+                    <button className="w-[1.5rem] h-[1.5rem] rounded-full border border-gray-500 bg-gradient-to-l from-pink-500 to-purple-700 text-white flex items-center justify-center">
                         <Check className=' w-[3vh]' />
                     </button>
                 ) : (
-                    <button onClick={() => props.handleClick(props.index)} className="w-[1.5rem] h-[1.5rem] rounded-full border flex items-center justify-center">
+                    <button  className="w-[1.5rem] h-[1.5rem] rounded-full border flex items-center justify-center">
                         
                     </button>
                 )
