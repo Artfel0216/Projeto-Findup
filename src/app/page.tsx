84ab3ec56b7bff48237ea5734b27036be103d658
Arthur Fellipe <arthur.fellipe.brandao@gmail.com>
2024-05-28 15:02:44 -0300

    atualização

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,73 @@
-"use client"
-import { useState } from "react"
+"use client";
+import { useState, useEffect } from "react";
 import { Line } from "../components/line";
 import TodoList from "@/components/TodoList";
 
-
-
 export default function Home() {
+  const [tasks, setTasks] = useState<{ checked: boolean; title: string }[]>(
+    JSON.parse(localStorage.getItem("todo") || "[]")
+  );
+  const [newTask, setNewTask] = useState<{ checked: boolean; title: string }>();
 
+  const handleClick = (index: number) => {
+    const oldTodos = tasks;
+    oldTodos[index].checked = !oldTodos[index].checked;
+    const newTodos = [...oldTodos];
 
-    
-    const [name, setName] = useState<string>("Arthur")
+    setTasks(newTodos);
+  };
 
-    const [todos, setTodos] = useState<any>([]);
+  function handleInputChange(event: any) {
+    setNewTask({ title: event.target.value, checked: false });
+  }
 
-    const handleClick = (index: number) => { 
-        const oldTodos = todos;
-        oldTodos[index].checked = !oldTodos[index].checked;
-        const newTodos = [...oldTodos];
+  function addTask(e: any) {
+    if (newTask) {
+      setTasks([...tasks, newTask]);
+      setNewTask(undefined);
 
-        setTodos(newTodos);
+      e.preventDefault("Enter tasks");
+      e.preventDefault([tasks])
     }
+}
 
-    const changeTodoList = (value: string) => {
-        const oldTodos = todos;
-        oldTodos.push({ title: value, checked: false });
-        const newTodos = [...oldTodos];
+function clearTasks(event: any) {
+    setTasks([]);
+}
 
-        setTodos(newTodos);
-    }
+  useEffect(() => {
+    console.log(tasks);
+    localStorage.setItem("todo", JSON.stringify(tasks));
+  }, [tasks]);
 
-    return (
-        <div>
+  return (
+    <div>
+      <TodoList
+        addTask={addTask}
+        handleInputChange={handleInputChange}
+        newTask={newTask}
+        clearTask={clearTasks}
+      />
 
-           <TodoList changeTodoList={changeTodoList} />
-        
-        
+      {/* primeiro container */}
 
+      <div className="flex gap-60 h-[15rem] bg-gradient-to-l from-pink-500 to-purple-700 text-white font-bold text-4xl">
+        <p className=" ml-[25rem] mt-[4rem]">T O D O</p>
+      </div>
 
-            {/* primeiro container */}
+      {/* segundo container container */}
 
-            <div className="flex gap-60 h-[15rem] bg-gradient-to-l from-pink-500 to-purple-700 text-white font-bold text-4xl">
-                <p className=" ml-[25rem] mt-[4rem]">
-                    T O D O 
-                </p>
-                
-                
-            </div>
-
-            {/* segundo container container */}
-
-            <div className="bg-zinc-800 w-[70vh] h-[57vh] absolute bottom-[8vh] ml-[26rem] rounded shadow-md">
-                {
-                    todos.map ((todo: any, index: number ) => (
-                        <Line 
-                            key={index}
-                            index={index}
-                            title={todo.title} 
-                            checked={todo.checked} 
-                            handleClick={handleClick} 
-                        />
-                    ))
-                }
-            </div>
-        </div>
-    );
+      <div className="bg-zinc-800 w-[70vh] h-[57vh] absolute bottom-[8vh] ml-[26rem] rounded shadow-md">
+        {tasks.map((todo: any, index: number) => (
+          <Line
+            key={index}
+            index={index}
+            title={todo.title}
+            checked={todo.checked}
+            handleClick={handleClick}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,52 +1,52 @@
-import { Input } from 'postcss'
-import input from 'postcss/lib/input'
-import React, {useState} from 'react'
 
-function TodoList ({changeTodoList} : {changeTodoList: (value:string) => void}) {
-    const [task, setTask] = useState(["arthur"])
-    const [newTask, setNewTask] = useState ("")
-  
+import React, { useState, useEffect } from "react";
+interface TodoListProps {
+  handleInputChange: (event: any) => void;
+  addTask: (event: any) => void;
+  newTask: { title: string; checked: boolean } | undefined;
+  clearTask: (event: any) => void;
+}
+function TodoList({
+  addTask,
+  handleInputChange,
+  newTask,
+  clearTask,
+}: TodoListProps) {
 
-    function handleInputChange (event: any) {
-      setNewTask(event.target.value)
-    }
+  return (
+    <form
+      id="todo-form"
+      className="absolute flex items-center mt-[10rem]"
+      >
+      <div className="flex items-center cursor-pointer">
 
-    function addTask (e:any) {
-      e.preventDefault("Enter task")
-      
+      <input
+        type="text"
+        className=" p-[15px] rounded ml-[26rem] w-[22rem] text-zinc-50 h-[2rem] bg-zinc-800 border-none absolute"
+        placeholder="Digite sua tarefa"
+        value={newTask?.title ?? ""}
+        onChange={handleInputChange}
+      />
 
-      if (newTask.trim() !== "") {
-        changeTodoList(newTask)
-      }
 
-      localStorage.setItem(newTask, "task")
-      setNewTask('')
-      
-    }
+      <a
+        id="dado"
+        className=" rounded flex items-center justify-center h-[2rem] absolute ml-[49rem] border-2 border-none w-[2rem]  bg-zinc-800 text-zinc-50"
+        onClick={addTask}
+      >
+        +
+      </a>
 
-    function salvar(newTask: string) {
-        localStorage.info = document.getElementById ("dado")
-      }
+      <a 
+      className=" cursor-pointer rounded flex items-center justify-center h-[2rem] absolute ml-[52rem] border-2 border-none w-[2rem]  bg-zinc-800 text-zinc-50"
+      onClick={clearTask}
+      >
+        x
+      </a>
 
-    return (
-      
-              <form id='todo-form' className="absolute flex items-center mt-[10rem]" 
-              onSubmit={addTask}>
-
-              <input  type="text" className=" p-[15px] rounded ml-[26rem] w-[22rem] text-zinc-50 h-[2rem] bg-zinc-800 border-none absolute"
-               
-               placeholder='Digite sua tarefa'
-               value={newTask}
-               onChange={handleInputChange}
-               />
-
-                <button id='dado' className=" rounded flex items-center justify-center h-[2rem] absolute ml-[49rem] border-2 border-none w-[2rem]  bg-zinc-800 text-zinc-50">
-                  +
-                </button>
-
-               </form>
-           
-    )
+      </div>
+    </form>
+  );
 }
 
-export default TodoList
+export default TodoList;
